fc61762f4837f50b4a00df7e9fd9f6ec0b51cd03
Rajasegar <rajasegarchandran@FWS-CHE-LT-4034.local>
2020-08-05 11:09:15 +0530

    chore: fix lint errors

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,33 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  extends: 'octane'
+  extends: "octane",
+  rules: {
+    "no-curly-component-invocation": {
+      allow: [
+        "day-js",
+        "dayjs-add",
+        "dayjs-calendar",
+        "dayjs-diff",
+        "dayjs-end-of",
+        "dayjs-format",
+        "dayjs-from-now",
+        "dayjs-from",
+        "dayjs-is-after",
+        "dayjs-is-before",
+        "dayjs-is-between",
+        "dayjs-is-leap-year",
+        "dayjs-is-same-or-after",
+        "dayjs-is-same-or-before",
+        "dayjs-is-same",
+        "dayjs-start-of",
+        "dayjs-subtract",
+        "dayjs-to-now",
+        "dayjs-to",
+      ],
+    },
+    "no-implicit-this": {
+      allow: ["day-js", "dayjs-calendar"],
+    },
+  },
 };
