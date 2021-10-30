5f516ebbd28af50e1a79f638d6905e650231db2c
Rajasegar <rajasegar.c@gmail.com>
2021-10-30 11:28:53 +0530

    chore(ci): remove unwanted ember try envs and make pr tests slim

--- config/ember-try.js
+++ config/ember-try.js
@@ -29,44 +29,6 @@ module.exports = async function () {
           },
         },
       },
-      // The default `.travis.yml` runs this scenario via `npm test`,
-      // not via `ember try`. It's still included here so that running
-      // `ember try:each` manually or from a customized CI config will run it
-      // along with all the other scenarios.
-      {
-        name: "ember-default",
-        npm: {
-          devDependencies: {},
-        },
-      },
-      {
-        name: "ember-default-with-jquery",
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            "jquery-integration": true,
-          }),
-        },
-        npm: {
-          devDependencies: {
-            "@ember/jquery": "^0.5.1",
-          },
-        },
-      },
-      {
-        name: "ember-classic",
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            "application-template-wrapper": true,
-            "default-async-observers": false,
-            "template-only-glimmer-components": false,
-          }),
-        },
-        npm: {
-          ember: {
-            edition: "classic",
-          },
-        },
-      },
     ],
   };
 };
